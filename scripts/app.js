9e704ab6f083af840fe42bc57f396d99c0d1b3d7
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-07-07 17:19:41 +0300

    added insert text function

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,11 @@ function init()
     const insertBtnElem = document.querySelector("#insertBtn");
     const insertBtnFrmElem = document.querySelector("#insertBtnFrm");
     const closeBtnFrmElem = document.querySelector("#closeBtnFrm");
+    const inputTitleElem = document.querySelector("#inputTitle");
+    const inputContentElem = document.querySelector("#inputContent");
 
     const insertFormElem = document.querySelector(".insertForm");
+    const sectionContentElem = document.querySelector(".sectionContent");
 
     function openInsertForm()
     {
@@ -16,8 +19,25 @@ function init()
         insertFormElem.classList.remove("open");
     }
 
+    function insertCardForm()
+    {
+        let content = inputContentElem.value;
+
+        // Replace newline characters with <br> tags
+        content = content.replace(/\n/g, '<br>');
+        
+        sectionContentElem.innerHTML += `<div class="card">
+                                            <h2>${inputTitleElem.value}</h2>
+                                            <hr>
+                                            <p>${content}</p>
+                                            <button class="editBtn">Edit</button>
+                                            <button class="deleteBtn">Delete</button>
+                                        </div>`
+    }
+
     insertBtnElem.addEventListener("click", openInsertForm);
     closeBtnFrmElem.addEventListener("click", closeInsertForm);
+    insertBtnFrmElem.addEventListener("click", insertCardForm);
 }
 
 document.addEventListener('DOMContentLoaded', init);
