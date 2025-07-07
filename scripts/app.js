86eaf0d96d14736520a5826f3f481809fd6f03dc
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-07-07 19:45:23 +0300

    still working on an edit functionality

--- scripts/app.js
+++ scripts/app.js
@@ -6,42 +6,63 @@ function init()
     const inputTitleElem = document.querySelector("#inputTitle");
     const inputContentElem = document.querySelector("#inputContent");
     let deleteBtnsElem = document.querySelectorAll(".deleteBtn");
+    let editBtnsElem = document.querySelectorAll(".editBtn");
 
     const insertFormElem = document.querySelector(".insertForm");
     const sectionContentElem = document.querySelector(".sectionContent");
 
     function openInsertForm()
     {
+        insertBtnFrmElem.textContent = "Insert";
         insertFormElem.classList.add("open");
     }
 
     function closeInsertForm()
     {
         insertFormElem.classList.remove("open");
+        checkForButtons();
     }
 
     function insertCardForm()
     {
-        let content = inputContentElem.value;
+        if(insertBtnFrmElem.textContent == "Insert")
+        {
+            let content = inputContentElem.value;
 
-        // this line is learned from the internet
-        content = content.replace(/\n/g, '<br>');
-        
-        sectionContentElem.innerHTML += `<div class="card">
-                                            <h2>${inputTitleElem.value}</h2>
-                                            <hr>
-                                            <p>${content}</p>
-                                            <button class="editBtn">Edit</button>
-                                            <button class="deleteBtn">Delete</button>
-                                        </div>`;
+            // this line is learned from the internet
+            content = content.replace(/\n/g, '<br>');
+            
+            sectionContentElem.innerHTML += `<div class="card">
+                                                <h2>${inputTitleElem.value}</h2>
+                                                <hr>
+                                                <p>${content}</p>
+                                                <button class="editBtn">Edit</button>
+                                                <button class="deleteBtn">Delete</button>
+                                            </div>`;
 
-        checkForButtons();
+            inputTitleElem.value = "";
+            inputContentElem.value = "";
+            checkForButtons();
+        }
     }
 
     function deleteCard(button)
     {
         const card = button.parentNode;
         card.remove();
+    }
+
+    function editCard(button)
+    {
+        const card = button.parentNode;
+
+        const title = card.querySelector(".title");
+        const content = card.querySelector(".content");
+
+        inputTitleElem.value = title.textContent;
+        inputContentElem.value = content.textContent;
+        openInsertForm();
+        insertBtnFrmElem.textContent = "Save";
     }
 
     function checkForButtons()
@@ -52,6 +73,15 @@ function init()
             button.addEventListener("click", function()
             {
                 deleteCard(this);
+            });
+        });
+
+        editBtnsElem = document.querySelectorAll(".editBtn");
+        editBtnsElem.forEach(button =>
+        {
+            button.addEventListener("click", function()
+            {
+                editCard(button);
             });
         });
     }
