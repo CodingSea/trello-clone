f18c1ace79cb87a83f730fcf6830b570981007ba
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-07-07 12:26:00 +0300

    added a popup div

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,23 @@
 function init()
 {
-    
+    const insertBtnElem = document.querySelector("#insertBtn");
+    const insertBtnFrmElem = document.querySelector("#insertBtnFrm");
+    const closeBtnFrmElem = document.querySelector("#closeBtnFrm");
+
+    const insertFormElem = document.querySelector(".insertForm");
+
+    function openInsertForm()
+    {
+        insertFormElem.classList.add("open");
+    }
+
+    function closeInsertForm()
+    {
+        insertFormElem.classList.remove("open");
+    }
+
+    insertBtnElem.addEventListener("click", openInsertForm);
+    closeBtnFrmElem.addEventListener("click", closeInsertForm);
 }
 
 document.addEventListener('DOMContentLoaded', init);
